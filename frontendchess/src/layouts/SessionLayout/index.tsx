e2ceacc4303923e118edd991d627a193f73c4e42
Juan Jose Vergara <40677444+jjvemz@users.noreply.github.com>
2024-11-27 15:21:07 -0300

    Session layout

---
+++ frontendchess/src/layouts/SessionLayout/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Header from '../../components/Header';
+import Footer from '../../components/Footer';
+
+const SessionLayout = () => {
+  return (
+    <>
+      <Header/>
+        <body>
+          
+        </body>
+      <Footer/>
+    </>
+  )
+}
+
+export default SessionLayout;
