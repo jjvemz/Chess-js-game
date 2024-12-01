2f73a7136b20aaa9a8ba9062b637fb747de39a1c
Juan Jose Vergara <40677444+jjvemz@users.noreply.github.com>
2024-12-01 17:20:23 -0300

    Chat interface implemented

--- frontendchess/src/components/Chat/index.tsx
+++ frontendchess/src/components/Chat/index.tsx
@@ -1,8 +1,36 @@
-import React from 'react'
+import {useState, useEffect} from 'react';
+import socket from '../../utils/sockets';
 
 const Chat = () => {
+  const [messages, setMessages] = useState([]);
+  const [newMessage, setNewMessage] = useState('');
+
+  useEffect(()=>{
+    socket.on('message', (message) =>{
+      setMessages([...messages, message]);
+    })
+  },[messages]);
+
+  const sendMessage = () =>{
+    socket.emit('message', newMessage);
+    setNewMessage('');
+  };
+
   return (
-    <div>Chat</div>
+    <div>
+      <div className="chat">
+          {messages.map((msg, index) =>(
+            <div key={index}>{msg}</div>
+          ))}
+      </div>
+      <div className="input">
+        <input 
+        type="text" 
+        value={newMessage}
+        onChange={(e) => setNewMessage(e.target.value)}/>
+        <button onClick={sendMessage}>Enviar mensaje</button>
+      </div>
+    </div>
   )
 }
 
