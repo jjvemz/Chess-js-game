face954bec2ea66a6047e50e3122e2a800db1487
Juan Jose Vergara <40677444+jjvemz@users.noreply.github.com>
2024-12-09 16:02:19 -0300

    New main page for the game initialized with form using MaterialUI

---
+++ frontendchess/src/Components/CustomDialogue/index.tsx
@@ -0,0 +1,24 @@
+import  Dialog  from "@mui/material/Dialog";
+import  DialogActions  from "@mui/material/DialogActions";
+import  DialogContent  from "@mui/material/DialogContent";
+import  DialogContentText  from "@mui/material/DialogContentText";
+import  DialogTitle  from "@mui/material/DialogTitle";
+
+export default function CustomDialog({ open, children, title, contentText, handleContinue }) {
+  return (
+    <Dialog open={open}> {/*dialog container*/}
+      <DialogTitle>{title}</DialogTitle>
+      <DialogContent> {/* Main body of modal/dialog */}
+        <DialogContentText> {/* main text */}
+          {contentText}
+        </DialogContentText>
+        {children} {/* Other content */}
+      </DialogContent>
+      <DialogActions> {/* Dialog action buttons */}
+        {/* Force users to make input without option to cancel */}
+        {/* <Button onClick={handleClose}>Cancel</Button> */}
+        <button onClick={handleContinue}>Continue</button>
+      </DialogActions>
+    </Dialog>
+  );
+}
---
+++ frontendchess/src/Components/Game/index.tsx
@@ -0,0 +1,71 @@
+import { useState, useMemo, useCallback, useEffect } from 'react';
+import { Chessboard } from "react-chessboard";
+import { Chess } from "chess.js";
+import CustomDialog from '../CustomDialogue';
+
+function Game({ players, room, orientation, cleanup }){
+    const chess = useMemo(()=> new Chess(), []);
+    const [fen, setFen] = useState(chess.fen());
+    const [over, setOver] = useState("");
+
+    const makeAMove = useCallback(
+        (move) => {
+            try {
+                const result = chess.move(move); // update Chess instance
+                setFen(chess.fen()); // update fen state to trigger a re-render
+
+                console.log("Se acabó, jaque mate", chess.isGameOver(), chess.isCheckmate());
+
+                if (chess.isGameOver()) { // check if move led to "game over"
+                    if (chess.isCheckmate()) { // if reason for game over is a checkmate
+                        // Set message to checkmate. 
+                        setOver(
+                            `Jaque mate! ${chess.turn() === "w" ? "Negras" : "Blancas"} Ganan!`
+                        );
+                        // The winner is determined by checking which side made the last move
+                    } else if (chess.isDraw()) { // if it is a draw
+                        setOver("Empate"); // set message to "Draw"
+                    } else {
+                        setOver("Game over");
+                    }
+                }
+
+                return result;
+            } catch (e) {
+                return null;
+            }
+        },
+        [chess]
+    );
+
+      const onDrop = (sourceSquare, targetSquare) => {
+        const moveData = {
+            from: sourceSquare,
+            to: targetSquare,
+            color: chess.turn(),
+        };
+
+        const move = makeAMove(moveData);
+
+        if (move === null) return false;
+
+        return true;
+    };
+
+    return(
+        <>
+        <div className="board">
+        <Chessboard id="BasicBoard" position={fen} onPieceDrop={onDrop} />  {/**  <- 4 */}
+      </div>
+      <CustomDialog 
+                open={Boolean(over)}
+                title={over}
+                contentText={over}
+                handleContinue={() => {
+                    setOver("");
+                } } children={undefined}      />
+        </>
+    )
+}
+
+export default Game;
